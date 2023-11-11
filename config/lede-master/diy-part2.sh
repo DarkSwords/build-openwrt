1f7b16b09925435d7af2ba047b0e7e9bdda542aa
M <28711483+DarkSwords@users.noreply.github.com>
2023-11-11 20:56:29 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -34,7 +34,8 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # echo 'src-git argone https://github.com/kenzok78/luci-theme-argone' >>feeds.conf.default
 
 # Themes
-echo 'src-git argone https://github.com/kenzok78/luci-theme-argone' >>feeds.conf.default
+# echo 'src-git argone https://github.com/kenzok78/luci-theme-argone' >>feeds.conf.default
+echo 'src-git argone https://github.com/jerrykuku/luci-theme-argon.git;branch=18.06' >> feeds.conf.default
 # Passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
