20e4c924b0f346d36dc4cd952eeddac02c0aafc6
M <28711483+DarkSwords@users.noreply.github.com>
2024-03-20 16:41:29 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -15,6 +15,6 @@
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/DarkSwords/openwrt-passwall' feeds.conf.default
-sed -i '$a src-gitpasswall-packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git opackages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 
 
